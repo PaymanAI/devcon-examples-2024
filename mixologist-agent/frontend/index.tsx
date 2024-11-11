9ee125a7b0522104543b06f686acab3780953567
Prashanth <prashanth@zoomie.com>
2024-11-10 22:48:33 -0800

    chore: update static images

--- mixologist-agent/frontend/index.tsx
+++ mixologist-agent/frontend/index.tsx
@@ -4,15 +4,34 @@ export const index = () => {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Buzz Lightbeer</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/public/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/public/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/public/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/public/site.webmanifest" />
+        <link rel="apple-touch-icon" href="logo192.png" />
+        <meta name="description" content="Get our bartender Buzzed Lightbeer to prepare a personalized drink for you" />
+        <title>Buzzed Lightbeer | Payman</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossorigin="true"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:wght@100..900&display=swap"
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:wght@100..900&display=swap"
           rel="stylesheet"
         />
         <script src="https://unpkg.com/htmx.org@1.9.2"></script>
